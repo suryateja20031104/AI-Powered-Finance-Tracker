fa950a438a52250c056e9537b71c6ab196b28435
surya <suryateja1938102074@gmail.com>
2026-05-10 13:56:35 +0530

    update base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: 'https://ai-powered-finance-tracker-yrmj.onrender.com/api'
 })
 
 API.interceptors.request.use((config) => {
